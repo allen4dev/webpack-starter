3a6e1667bacdde67c9abc93511007d6125df30b9
allen4dev <alanaliagadev@gmail.com>
2019-03-15 23:10:03 -0400

    [Add] code coverage with jest

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   ...commonConfig,
   // 1 hour to figure to projects fails only one project is passed
   projects: ['<rootDir>/test/jest.client.js', '<rootDir>/test/jest.lint.js'],
+  collectCoverageFrom: ['**/src/**/*.js'],
 };
