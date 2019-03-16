c44a53d2a403e8c4dde32cd48497d7ea80502479
allen4dev <alanaliagadev@gmail.com>
2019-03-15 23:01:04 -0400

    [Add] enhance jest watch mode with jest-watch-typeahead and jest-watch-select-project

--- test/jest.common.js
+++ test/jest.common.js
@@ -7,4 +7,10 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/test/__mocks__/fileMock.js',
   },
+
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+    'jest-watch-select-projects',
+  ],
 };
